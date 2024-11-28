89328a2a5f387a44b74ca163187f44cdd31ee5dd
Bea <beaalyssandracastro@outlook.com>
2024-11-28 12:57:13 -0500

    updating files for testing

--- PP2/pages/_app.tsx
+++ PP2/pages/_app.tsx
@@ -1,30 +1,41 @@
-// import React from 'react';
-// import { AppProps } from 'next/app';
-// import '../styles/styles.css';
-// import '../styles/globals.css';
-// import Header from './header'; // Ensure this path is correct
-// import { AuthProvider } from '../auth-context'; // Update the path to your AuthContext file
+import React from 'react';
+import { AppProps } from 'next/app';
+import '../styles/styles.css';
+import '../styles/globals.css';
+import Header from './header'; // Ensure this path is correct
+import { AuthProvider } from './auth-context'; // Update the path to your AuthContext file
+import Input from './frontend/code-writing-and-execution/input'; 
+import ModifyTemplate from './frontend/code-templates/modify-template';
+import { useRouter } from 'next/router';
 
-// const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-//     const [darkMode, setDarkMode] = React.useState(false);
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+    const [darkMode, setDarkMode] = React.useState(false);
+
+    const router = useRouter();
     
-//     const toggleDarkMode = () => {
-//         setDarkMode(!darkMode);
-//     };
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+    };
 
-//   return (
-//         <div className={`${darkMode && "dark"}`}>
-//             <AuthProvider>
-//                 <div>
-//                     {/* Persistent Header */}
-//                     <Header toggleDarkMode={toggleDarkMode} darkMode={darkMode}/>
+  return (
+        <div className={`${darkMode && "dark"}`}>
+            <AuthProvider>
+                <div>
+                    {/* Persistent Header */}
+                    <Header toggleDarkMode={toggleDarkMode} darkMode={darkMode}/>
                     
-//                     {/* Dynamic Page Content */}
-//                     <Component {...pageProps} />
-//                 </div>
-//             </AuthProvider>
-//         </div>
-//     );
-// };
+                    {/* Conditionally Render Input */}
+                    {router.pathname === "/frontend/code-writing-and-execution/input" ? (
+                        <Input darkMode={darkMode} />
+                    ) : router.pathname === "/frontend/code-templates/modify-template" ? (
+                        <ModifyTemplate darkMode={darkMode}/>
+                    ) : (   
+                        <Component {...pageProps} darkMode={darkMode} />
+                    )}
+                </div>
+            </AuthProvider>
+        </div>
+    );
+};
 
-// export default MyApp;
+export default MyApp;
